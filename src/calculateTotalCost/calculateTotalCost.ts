7805480c9373bae4860df2d0709d924744dabdc9
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-04-09 15:14:15 -0400

    Refactore: Updated the production code to show an error message when the paint cost and labor cost is negative values

--- src/calculateTotalCost/calculateTotalCost.ts
+++ src/calculateTotalCost/calculateTotalCost.ts
@@ -1,3 +1,6 @@
-export default function calculateTotalCost(paintCost: number, laborCost: number): number {
+export default function calculateTotalCost(paintCost: number, laborCost: number): number | string {
+  if (paintCost < 0 || laborCost < 0) {
+    return "Error: Paint cost and labor cost must be non-negative.";
+  }
   return paintCost + laborCost;
 }

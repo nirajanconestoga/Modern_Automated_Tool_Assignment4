0a48e6c13e82b46b419f6a18f328a7c56bd258a4
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-04-09 15:25:20 -0400

    Updated the production to to handle a negative square feet to throw error message

--- src/convertAreaToSquareFeet/convertAreaToSquareFeet.ts
+++ src/convertAreaToSquareFeet/convertAreaToSquareFeet.ts
@@ -1,4 +1,7 @@
 export default function convertAreaToSquareFeet(area: number): number {
+  if (area < 0) {
+    throw new Error('Area cannot be negative');
+  }
   const conversionFactor = 10.7639;
   return area * conversionFactor;
 }

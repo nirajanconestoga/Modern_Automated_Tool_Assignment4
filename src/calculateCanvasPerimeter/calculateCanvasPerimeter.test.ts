a9f70e1f3056e2d498baf6e27e722c1290ac8f1a
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-04-09 14:36:21 -0400

    Updated the message in the describe block as well

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
@@ -1,8 +1,9 @@
 import calculateCanvasPerimeter from "./calculateCanvasPerimeter";
 
-describe("calculateCanvasSize", () => {
+describe("calculateCanvasPerimeter", () => {
   it("Verify that calculateCanvasPerimeter should return the correct canvas size for positive length and width", () => {
     const result = calculateCanvasPerimeter("10", "100");
     expect(result).toEqual(220);
   });
+  
 });

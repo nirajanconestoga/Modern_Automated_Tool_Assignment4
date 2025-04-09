854c6311bb528f77453b20b537c32f59fd70aa17
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-04-09 14:19:18 -0400

    test: ensure calculateCanvasDiagonal returns correct diagonal when length is zero and width is positive

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -4,4 +4,7 @@ describe('calculateCanvasDiagonal', () => {
   it('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is positive and width is zero', () => {
     expect(calculateCanvasDiagonal('3', '0')).toBe(3);
   })
+  it ('Verify that calculateCanvasDiagonal should calculate the correct diagonal when length is zero and width is positive', () => {
+    expect(calculateCanvasDiagonal('0', '4')).toBe(4);
+  });
 });

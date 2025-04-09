f6e905ca321cbd5bb2dd3c2ab65c32a2c64758ad
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-04-09 15:06:24 -0400

    Refactore: Updated the production code to show an error message when the cost and point required is a negative value

--- src/calculatePaintCost/calculatePaintCost.ts
+++ src/calculatePaintCost/calculatePaintCost.ts
@@ -1,3 +1,6 @@
-export default function calculatePaintCost(paintRequired: number, costPerLiter: number): number {
+export default function calculatePaintCost(paintRequired: number, costPerLiter: number): number | string {
+  if (paintRequired < 0 || costPerLiter < 0) {
+    return "Error: Paint required and cost per liter must be non-negative.";
+  }
   return paintRequired * costPerLiter;
 }
